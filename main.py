236f9e75bb1e92e08172dde93c363d86c30c430d
dezsim <dezsimartin@gmail.com>
2024-10-06 20:35:50 +0200

    nit formatting

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ load_dotenv()
 def get_env(env_var):
     value = os.getenv(env_var)
     if value is None:
-        raise KeyError(f"Environment variable {env_var} is not set.")
+        raise KeyError(f'Environment variable {env_var} is not set.')
     return value
 
 try:
     rpc_url = get_env('RPC_URL')
     tornado_cash_address = get_env('TORNADO_CASH_ADDRESS')
-    print(f"RPC URL: {rpc_url}")
-    print(f"Investigated Tornado Cash Address: {tornado_cash_address}")
+    print(f'RPC URL: {rpc_url}')
+    print(f'Investigated Tornado Cash Address: {tornado_cash_address}')
     print('-' * 70)
 except KeyError as e:
-    print(f"Error: {e}")
+    print(f'Error: {e}')
     exit(1)
 
 w3 = Web3(Web3.HTTPProvider(rpc_url))
@@ -31,10 +31,10 @@ overallEthValue: float = 0.0
 def get_block_number():
     while True:
         try:
-            block_number = int(input("Enter the Ethereum block number: "))
+            block_number = int(input('Enter the Ethereum block number: '))
             return block_number
         except ValueError:
-            print("Invalid input. Please enter a valid integer for the block number.")
+            print('Invalid input. Please enter a valid integer for the block number.')
 
 def process_tx(transaction, txHash: str, top_level: bool =True):
     if top_level:
@@ -50,10 +50,10 @@ def process_tx(transaction, txHash: str, top_level: bool =True):
                 global overallEthValue
                 overallEthValue += float(currentEthValue)
                 print('Tornado Cash transaction detected:')
-                print(f" - Transaction Hash: {txHash}")
-                print(f" - From: {child_tx.get('from')}")
-                print(f" - To: {child_tx.get('to')}")
-                print(f" - Value: {currentEthValue} ETH")
+                print(f' - Transaction Hash: {txHash}')
+                print(f' - From: {child_tx.get('from')}')
+                print(f' - To: {child_tx.get('to')}')
+                print(f' - Value: {currentEthValue} ETH')
                 print('-' * 70)
             process_tx(child_tx, txHash, False)
 
@@ -65,27 +65,27 @@ def process_block(block_number):
     blockTransactions = blockResponse.get('result')
     
     if not blockTransactions:
-        print("No transaction traces found in the block.")
+        print('No transaction traces found in the block.')
         return
 
     for transaction in blockTransactions:
         txHash = transaction.get('txHash')
         process_tx(transaction, txHash)
         
-    print(f"Completed scanning for Tornado Cash transfers in block #{block_number}.")
+    print(f'Completed scanning for Tornado Cash transfers in block #{block_number}.')
     
     if suspiciousTxCount == 0:
-        print("No Tornado Cash transfer detected.")
+        print('No Tornado Cash transfer detected.')
     else:
-        print(f"Detected {suspiciousTxCount} Tornado Cash transfers in block #{block_number}.")
-        print(f"Total value of suspicious transactions: {overallEthValue} ETH")
+        print(f'Detected {suspiciousTxCount} Tornado Cash transfers in block #{block_number}.')
+        print(f'Total value of suspicious transactions: {overallEthValue} ETH')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     block_number = get_block_number()
 
     if w3.is_connected():
-        print(f"Connected to Ethereum node. Scanning block {block_number}..")
+        print(f'Connected to Ethereum node. Scanning block {block_number}..')
         process_block(block_number)
     else:
-        print("Failed to connect to Ethereum node.")
+        print('Failed to connect to Ethereum node.')
 
